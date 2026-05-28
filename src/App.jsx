b3e81e8256d43581755d07ed387bf635a86fe5a0
Astellar <stellarblue05@gmail.com>
2026-05-28 20:49:09 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Start from './Start.jsx'
+function App(){
+  return(
+    <Start/>
+  )
+}
+
+export default App;
---
+++ src/Systems/Lilum.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lilum = () => {
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    );
+}
+
+export default Lilum;
